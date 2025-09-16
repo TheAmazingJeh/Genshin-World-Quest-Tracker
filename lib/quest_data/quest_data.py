4dd0051999c7e228f721528735744c579a410f23
Theamazingjeh <joehbooth2004@gmail.com>
2025-09-16 22:27:08 +0100

    Use correct image for craftable item creation items.

--- lib/quest_data/quest_data.py
+++ lib/quest_data/quest_data.py
@@ -74,9 +74,18 @@ def get_quest_rewards(soup:BeautifulSoup):
         current_reward['Link'] = inner_span.select_one('a')['href']
         # Check if there is an image tag in the span
         if inner_span.select_one('a > img'):
-            # Get the image of the item using the data-src parameter of the img tag, or the src parameter if the data-src parameter is not found
-            try: img = inner_span.select_one('a > img')['data-src']
-            except KeyError: img = inner_span.select_one('a > img')['src']
+            # Check to see if the item is a diagram.
+            
+            if inner_span.select_one('a')['title'].startswith("Diagram: "):
+                img = "https://static.wikia.nocookie.net/gensin-impact/images/1/15/Item_Diagram.png"
+            elif inner_span.select_one('a')['title'].startswith("Recipe: "):
+                img = "https://static.wikia.nocookie.net/gensin-impact/images/a/a5/Item_Recipe.png"
+            elif inner_span.select_one('a')['title'].startswith("Instructions: "):
+                img = "https://static.wikia.nocookie.net/gensin-impact/images/e/ed/Item_Instructions.png"
+            else:
+                # Get the image of the item using the data-src parameter of the img tag, or the src parameter if the data-src parameter is not found
+                try: img = inner_span.select_one('a > img')['data-src']
+                except KeyError: img = inner_span.select_one('a > img')['src']
         else:
             img = "https://static.wikia.nocookie.net/gensin-impact/images/f/f8/Icon_Emoji_Paimon%27s_Paintings_02_Qiqi_1.png"
         current_reward['Image'] = get_image_path(img)
